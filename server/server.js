469056ae645876c3a9e5af6b00eddd3b399b8a96
Jens <jens@jenstech.com>
2017-09-26 23:07:42 +0200

    Add DELETE and PATCH /todo/:id routes and tests

--- server/server.js
+++ server/server.js
@@ -58,11 +58,37 @@ app.delete('/todos/:id', (req, res) => {
         if(!todo){
             return res.status(404).send();
         }
-        res.send(todo);
+        res.send({todo});
     }).catch((e) =>{
         res.status(400).send();
     });
 });
+
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+  
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+  
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+  
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+  
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    })
+  });
 
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
