e2878599b9beca5860196d9530c0f196113717f5
Jens <jens@jenstech.com>
2017-09-26 22:58:22 +0200

    Add Delete /todos/:id route

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,42 @@
+const {ObjectID} = require('mongodb');
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+// var id = '59ca94a0291b71b442aa7d6a';
+
+// if(!ObjectID.isValid(id)){
+//     console.log('Id not valid');
+// }
+
+// Todo.find({
+//     _id: id
+// }).then((todos)=>{
+//     console.log('Todos', todos)
+// });
+
+
+// Todo.findOne({
+//     _id: id
+// }).then((todo)=>{
+//     console.log('Todo', todo)
+// });
+
+// Todo.findById(id).then((todo)=>{
+//     if(!todo){
+//         return console.log('Id not found');
+//     }
+//     console.log('Todo', todo)
+// }).catch((e) => {
+//     console.log(e);
+// });
+
+var id = "";
+User.findById(id).then((todo)=>{
+        if(!todo){
+            return console.log('Id not found');
+        }
+        console.log('Todo', todo)
+    }).catch((e) => {
+        console.log(e);
+    });
