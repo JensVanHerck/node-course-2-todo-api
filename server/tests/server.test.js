964e7c0ef1c32bab23622e7446a9c7d7b3a6304f
Jens <jens@jenstech.com>
2017-09-30 17:36:29 +0200

    clear

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -171,6 +171,7 @@ describe('DELETE /todos/:id', () => {
         })
         .end(done);
     });
+    });
     describe('GET /users/me', () => {
         it('should return user if authenticated', (done) => {
           request(app)
@@ -295,4 +296,21 @@ describe('DELETE /todos/:id', () => {
             });
     });
   });
-});
+  describe('DELETE /users/me/token', () => {
+    it('should remove auth token on logout', (done) => {
+      request(app)
+        .delete('/users/me/token')
+        .set('x-auth', users[0].tokens[0].token)
+        .expect(200)
+        .end((err, res) => {
+          if (err) {
+            return done(err);
+          }
+  
+          User.findById(users[0]._id).then((user) => {
+            expect(user.tokens.length).toBe(0);
+            done();
+          }).catch((e) => done(e));
+        });
+    });
+  });
